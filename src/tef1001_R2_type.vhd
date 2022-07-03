63ed2ddae7206aa99def2d784646cb9d85428f78
Geoff Jones <geoffhjones@msn.com>
2022-07-03 14:46:44 -0700

    Add comments to tef1001_R2_type.vhd

--- src/tef1001_R2_type.vhd
+++ src/tef1001_R2_type.vhd
@@ -1,7 +1,13 @@
+------------------------------------------------------------------------
+-- File tef1001_R2_type.vhd
+-- What does this name mean?
+-- What is this package for?
+-- Change name to hw_type_pkg.vhd? or hw_ident_pkg.vhd?
+------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-use IEEE.NUMERIC_STD.all;
+use     IEEE.STD_LOGIC_1164.all;
+use     IEEE.NUMERIC_STD.all;
 
 package hw_type_pkg is
-    constant c_HW_IDENT : std_logic_vector(7 downto 0) := x"02";
+    constant C_HW_IDENT     : std_logic_vector(7 downto 0) := X"02";    -- Add comment ?
 end hw_type_pkg;
